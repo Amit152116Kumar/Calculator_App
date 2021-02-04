4950f4f7a7f6d22340fc2b9b68057d439a1927bb
Amit Kumar <amit170103004@iitg.ac.in>
2021-02-05 00:05:14 +0530

    Added Subtraction Operation

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculator
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.os.Vibrator
@@ -8,44 +9,49 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
 
-const val PI: Double= 3.14159
-var output_result: String=""
-lateinit var binding:ActivityMainBinding
+const val PI: Double = 3.14159
+var output_result: String = ""
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var vibrator:Vibrator
-//    TO STORE ALL OPERATIONS AND SHOW ON EDIT TEXT
-    private var sum=""
-//    TO STORE CLICKED BUTTON INPUT
-    private var inputText=""
-//    FOR THE OPERATION BUTTON
-    private var pushButton=""
-//    TO STORE ONE DIGIT
-    private var sumNumber=""
-//    FOR MAX LIMIT OF ONE DIGIT IN EDIT TEXT
-    private var countNumber:Long=0
-//    TO NOT ADD TWO OPERATIONS CONTINUOUSLY IN CALCULATOR
-    private var flag=0
-//    FOR OPERATION CLASS INSTANCE
-    private lateinit var operation:Operations
+    private lateinit var vibrator: Vibrator
 
+    //    TO STORE ALL OPERATIONS AND SHOW ON EDIT TEXT
+    private var sum = ""
+
+    //    TO STORE CLICKED BUTTON INPUT
+    private var inputText = ""
+
+    //    FOR THE OPERATION BUTTON
+    private var pushButton = ""
+
+    //    TO STORE ONE DIGIT
+    private var sumNumber = ""
+
+    //    FOR MAX LIMIT OF ONE DIGIT IN EDIT TEXT
+    private var countNumber: Long = 0
+
+    //    TO NOT ADD TWO OPERATIONS CONTINUOUSLY IN CALCULATOR
+    private var flag = 0
+
+    //    FOR OPERATION CLASS INSTANCE
+    private lateinit var operation: Operations
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        vibrator= getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        operation=Operations(binding)
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        operation = Operations(binding)
 
         binding.angleDegree.setOnClickListener {
             vibrator.vibrate(30)
-            if (binding.angleDegree.text.toString()==getString(R.string.deg)){
+            if (binding.angleDegree.text.toString() == getString(R.string.deg)) {
                 binding.angleDegree.setText(R.string.rad)
-            }
-            else if (binding.angleDegree.text.toString()==getString(R.string.rad)){
+            } else if (binding.angleDegree.text.toString() == getString(R.string.rad)) {
                 binding.angleDegree.setText(R.string.deg)
             }
         }
@@ -55,60 +61,108 @@ class MainActivity : AppCompatActivity() {
 
     fun plusOnclickListener(view: View) {
         vibrator.vibrate(30)
-        pushButton="+"
-        flag=1
-        countNumber=0
-        sumNumber=""
-        if (sum!=""){
-            output_result=binding.outputText.text.toString()
-            inputText=binding.plus.text as String
-            sum+=inputText
+        pushButton = "+"
+        flag = 1
+        countNumber = 0
+        sumNumber = ""
+        if (sum != "") {
+            output_result = binding.outputText.text.toString().removePrefix("=")
+            inputText = binding.plus.text as String
+            sum += inputText
             binding.editText.setText(sum)
-            Log.d("TAG", "plusOnclickListener: ${output_result}")
+            Log.d("TAG", "plusOnclickListener: $output_result")
         }
     }
 
+    fun minusOnclickListener(view: View) {
+        vibrator.vibrate(30)
+        pushButton = "-"
+        flag = 1
+        countNumber = 0
+        sumNumber = ""
+        if (sum != "") {
+            output_result = binding.outputText.text.toString().removePrefix("=")
+            inputText = binding.minus.text as String
+            sum += inputText
+            binding.editText.setText(sum)
+            Log.d("TAG", "minusOnclickListener: $output_result")
+        }
+    }
+
+    fun multiplyOnclickListener(view: View) {
+        vibrator.vibrate(30)
+        pushButton = "*"
+        flag = 1
+        countNumber = 0
+        sumNumber = ""
+        if (sum != "") {
+            output_result = binding.outputText.text.toString().removePrefix("=")
+            inputText = binding.multiply.text as String
+            sum += inputText
+            binding.editText.setText(sum)
+            Log.d("TAG", "multiplyOnclickListener: ${output_result}")
+        }
+    }
+
+    fun divideOnclickListener(view: View) {
+        vibrator.vibrate(30)
+        pushButton = "/"
+        flag = 1
+        countNumber = 0
+        sumNumber = ""
+        if (sum != "") {
+            output_result = binding.outputText.text.toString().removePrefix("=")
+            inputText = binding.divide.text as String
+            sum += inputText
+            binding.editText.setText(sum)
+            Log.d("TAG", "divideOnclickListener: ${output_result}")
+        }
+    }
+
+
+    @SuppressLint("SetTextI18n")
     fun nineOnclickListener(view: View) {
         vibrator.vibrate(30)
         operation.limit_checker()
-        flag=0
-        if (countNumber<=18) {
+        flag = 0
+        if (countNumber <= 18) {
 
             inputText = binding.nine.text as String
-            sumNumber+=inputText
+            sumNumber += inputText
             sum += inputText
             binding.editText.setText(sum)
-            if (pushButton == "+")   { binding.outputText.text=operation.addition(sumNumber) }
-
-            else if (pushButton=="-")     { binding.outputText.text= operation.subtraction(sumNumber) }
-
-            else if (pushButton=="*")     { binding.outputText.text= operation.multiplication(sumNumber) }
-
-            else if (pushButton=="/")       { binding.outputText.text= operation.division(sumNumber) }
-
-            else {
+            if (pushButton == "+") {
+                binding.outputText.text = "=${operation.addition(sumNumber)}"
+            } else if (pushButton == "-") {
+                binding.outputText.text = "=${operation.subtraction(sumNumber)}"
+            } else if (pushButton == "*") {
+                binding.outputText.text = "=${operation.multiplication(sumNumber)}"
+            } else if (pushButton == "/") {
+                binding.outputText.text = "=${operation.division(sumNumber)}"
+            } else {
                 Log.d("TAG", "nineOnclickListener: else")
-                binding.outputText.text = sum
+                binding.outputText.text = "=$sum"
             }
             countNumber++
         }
     }
 
+    @SuppressLint("SetTextI18n")
     fun dotOnclickListener(view: View) {
         vibrator.vibrate(30)
-        flag=0
-        if (!sumNumber.contains(".")){
+        flag = 0
+        if (!sumNumber.contains(".")) {
             operation.limit_checker()
-            if (countNumber<=18) {
+            if (countNumber <= 18) {
                 inputText = binding.dot.text as String
-                sumNumber+=inputText
+                sumNumber += inputText
                 sum += inputText
                 binding.editText.setText(sum)
                 if (pushButton == "+") {
                     operation.addition(sumNumber)
                 } else {
                     Log.d("TAG", "dotOnclickListener: else")
-                    binding.outputText.text = sum
+                    binding.outputText.text = "=$sum"
                 }
                 countNumber++
             }
@@ -119,31 +173,32 @@ class MainActivity : AppCompatActivity() {
         vibrator.vibrate(30)
         binding.editText.textSize = 50F
         binding.outputText.textSize = 50F
-        countNumber=0
-        flag=0
-        output_result=""
-        sum=""
-        inputText=""
-        pushButton=""
+        countNumber = 0
+        flag = 0
+        output_result = ""
+        sum = ""
+        inputText = ""
+        pushButton = ""
         binding.editText.setText("")
         binding.outputText.text = "0"
     }
 
+    @SuppressLint("SetTextI18n")
     fun zeroOnclickListener(view: View) {
         vibrator.vibrate(30)
-        flag=0
-        if (sumNumber.isNotEmpty()){
+        flag = 0
+        if (sumNumber.isNotEmpty()) {
             operation.limit_checker()
-            if (countNumber<=18) {
+            if (countNumber <= 18) {
                 inputText = binding.zero.text as String
-                sumNumber+=inputText
+                sumNumber += inputText
                 sum += inputText
                 binding.editText.setText(sum)
                 if (pushButton == "+") {
                     operation.addition(sumNumber)
                 } else {
                     Log.d("TAG", "zeroOnclickListener: else")
-                    binding.outputText.text = sum
+                    binding.outputText.text = "=$sum"
                 }
                 countNumber++
             }
@@ -153,14 +208,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("SetTextI18n")
     fun deleteOnclickListener(view: View) {
-        if (sum==sumNumber){
+        if (sum == sumNumber) {
             sum.removeRange(sum.length - 1, sum.length)
             binding.editText.setText(sum)
             sumNumber.removeRange(sumNumber.length - 1, sumNumber.length)
-            binding.outputText.text=sumNumber
-        }
-        else{
+            binding.outputText.text = "=$sumNumber"
+        } else {
 
         }
     }
@@ -173,6 +228,8 @@ class MainActivity : AppCompatActivity() {
     fun sixOnclickListener(view: View) {}
     fun sevenOnclickListener(view: View) {}
     fun eightOnclickListener(view: View) {}
+    fun percentOnclickListener(view: View) {}
+    fun equalOnclickListener(view: View) {}
 
 
 }
--- app/src/main/java/com/example/calculator/Operations.kt
+++ app/src/main/java/com/example/calculator/Operations.kt
@@ -3,14 +3,13 @@ package com.example.calculator
 import android.util.Log
 import com.example.calculator.databinding.ActivityMainBinding
 import kotlin.math.abs
-import kotlin.math.log
 
 class Operations(private val binding: ActivityMainBinding) {
 
 
     fun addition(number: String): String {
 
-        var result = ""
+        var result: String
         var num1 = number
         var num2 = output_result
 
@@ -21,8 +20,8 @@ class Operations(private val binding: ActivityMainBinding) {
         if (!num2.contains(".")) {
             num2 = num2.plus(".0")
         }
-        var list1 = num1.split(".").toMutableList()
-        var list2 = num2.split(".").toMutableList()
+        val list1 = num1.split(".").toMutableList()
+        val list2 = num2.split(".").toMutableList()
 //        ------------------------------------------------
 
 //       MAKING DECIMAL PART EQUAL
@@ -64,40 +63,140 @@ class Operations(private val binding: ActivityMainBinding) {
         list1[1] = list1[1].reversed()
         list2[1] = list2[1].reversed()
         var carry = 0
-        var resultFloat=""
+        var resultFloat = ""
         for (i in list1[1].indices) {
-            val sum = (list1[1][i].toInt() - 48) + (list2[1][i].toInt() - 48)+carry
+            val sum = (list1[1][i].toInt() - 48) + (list2[1][i].toInt() - 48) + carry
             carry = sum / 10
             resultFloat += (sum % 10).toString()
         }
-        resultFloat=resultFloat.reversed()
+        resultFloat = resultFloat.reversed()
 
 //        ADDING THE Integral PART
 
         var resultInt = ""
         for (i in list1[0].indices) {
-            val sum = (list1[0][i].toInt() - 48) + (list2[0][i].toInt() - 48)+carry
+            val sum = (list1[0][i].toInt() - 48) + (list2[0][i].toInt() - 48) + carry
             carry = sum / 10
             resultInt += (sum % 10).toString()
         }
         if (carry != 0) {
             resultInt += carry
         }
-        resultInt=resultInt.reversed()
+        resultInt = resultInt.reversed()
 
 
-        result=resultInt.plus(".")
-        result=result.plus(resultFloat)
+        result = resultInt.plus(".")
+        result = result.plus(resultFloat)
 
-        result=result.removeSuffix(".0")
+        result = result.removeSuffix(".0")
         Log.d("TAG", "addition: $resultInt \t $resultFloat")
         return result
     }
 
 
-    fun multiplication(number: String):String {
-        var num1 = number
-        var num2 = output_result
+    fun subtraction(number: String): String {
+        var result: String
+        var num1 = output_result
+        var num2 = number
+
+//       IF ONE OF THE NUMBERS ARE INT MAKING THEM FLOAT
+        if (!num1.contains(".")) {
+            num1 = num1.plus(".0")
+        }
+        if (!num2.contains(".")) {
+            num2 = num2.plus(".0")
+        }
+
+//        TO DEFINE THE SIGN OF RESULT
+        var sign = ""
+        if (num1.toDouble() < num2.toDouble()) {
+            sign = "-"
+            val temp = num1
+            num1 = num2
+            num2 = temp
+        }
+        val list1 = num1.split(".").toMutableList()
+        val list2 = num2.split(".").toMutableList()
+
+//       MAKING DECIMAL PART EQUAL
+        val n = list1[1].length
+        val m = list2[1].length
+        var diff = abs(n - m)
+        if (n < m) {
+            for (i in 0 until diff) {
+                list1[1] = list1[1].plus("0")
+            }
+        } else {
+            for (i in 0 until diff) {
+                list2[1] = list2[1].plus("0")
+            }
+        }
+//        ------------------------------------------------
+
+//       MAKING INTEGRAL PART EQUAL
+        val a = list1[0].length
+        val b = list2[0].length
+        list1[0] = list1[0].reversed()
+        list2[0] = list2[0].reversed()
+        diff = abs(a - b)
+        if (a < b) {
+            for (i in 0 until diff) {
+                list1[0] = list1[0].plus("0")
+            }
+        } else {
+            for (i in 0 until diff) {
+                list2[0] = list2[0].plus("0")
+            }
+        }
+        Log.d("TAG", "subtraciton: list1 = $list1  \t list2 = $list2")
+//        SUBTRACTING THE DECIMAL PART
+        list1[1] = list1[1].reversed()
+        list2[1] = list2[1].reversed()
+        var carry = 0
+        var resultFloat = ""
+        for (i in list1[1].indices) {
+            var sub = (list1[1][i].toInt() - 48) - (list2[1][i].toInt() - 48) - carry
+            if (sub < 0) {
+                sub += 10
+                carry = 1
+            } else {
+                carry = 0
+            }
+
+            resultFloat += sub.toString()
+        }
+        resultFloat = resultFloat.reversed()
+        Log.d("TAG", "subtraction:  resultFloat = $resultFloat")
+
+//        ADDING THE Integral PART
+
+        var resultInt = ""
+        for (i in list1[0].indices) {
+            var sub = (list1[0][i].toInt() - 48) - (list2[0][i].toInt() - 48) - carry
+            if (sub < 0) {
+                sub += 10
+                carry = 1
+            } else {
+                carry = 0
+            }
+
+            resultInt += sub.toString()
+        }
+
+        resultInt = resultInt.plus(sign)
+        resultInt = resultInt.reversed()
+        Log.d("TAG", "subtraction: resultInt = $resultInt  ")
+
+        result = resultInt.plus(".")
+        result = result.plus(resultFloat)
+        Log.d("TAG", "subtraction: $result")
+        result = result.removeSuffix(".0")
+        return result
+    }
+
+
+    fun multiplication(number: String): String {
+
         var result = ""
 
         return result
@@ -110,12 +209,8 @@ class Operations(private val binding: ActivityMainBinding) {
         }
     }
 
-    fun subtraction(number: String):String{
-        var result = ""
 
-        return result
-    }
-    fun division(number: String):String{
+    fun division(number: String): String {
         var result = ""
 
         return result
